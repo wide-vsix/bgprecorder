6f0f57290b0c4c8ca0bb46f5bea6e60e246c730a
yas-nyan <yas-nyan@sfc.wide.ad.jp>
2022-07-14 15:34:13 +0900

    moduled

--- bgprecorder/util.py
+++ bgprecorder/util.py
@@ -1,17 +1,10 @@
-import psycopg2
-from psycopg2.extras import DictCursor
-import os
-import pathlib
-from datetime import datetime
-import argparse
-import sys
-import glob
-import time
-import ipaddress
-from datetime import datetime, date, timedelta
 import json
+from datetime import datetime, date
+import os
+import psycopg2
+import ipaddress
 
-DATETIME_FORMAT = '%Y%m%d%H%M'
+from psycopg2.extras import DictCursor
 
 
 def json_serial_default(obj):
@@ -20,6 +13,9 @@ def json_serial_default(obj):
         return obj.isoformat()
     # 上記以外はサポート対象外.
     raise TypeError("Type %s not serializable" % type(obj))
+
+
+DATETIME_FORMAT = '%Y%m%d%H%M'
 
 
 def connect():
@@ -79,16 +75,6 @@ def get_routes_from_address_and_datetime(address: str, target_datetime: datetime
     return get_routes_from_address(address=address, table_name=table_name)
 
 
-def get_args():
-    parser = argparse.ArgumentParser(
-        description='This is sample argparse script')
-    parser.add_argument('-a', '--address', type=str,
-                        help='target address', required=True)
-    parser.add_argument('-d', '--datetime', default=None, type=str,
-                        help=f'target datetime. example: "200601021504"')
-    return parser.parse_args()
-
-
 def longest_match(routes) -> list:
     longest_prefix = 0
     for route in routes:
@@ -96,19 +82,3 @@ def longest_match(routes) -> list:
         if prefixlen >= longest_prefix:
             longest_prefix = prefixlen
     return [route for route in routes if ipaddress.ip_network(route["nlri"]).prefixlen == longest_prefix]
-
-
-if __name__ == "__main__":
-    args = get_args()
-
-    address = args.address
-    if not args.datetime:
-        dt = datetime.now()
-    else:
-        dt = datetime.strptime(args.datetime, DATETIME_FORMAT)
-
-    routes = get_routes_from_address_and_datetime(
-        address=address, target_datetime=dt)
-    matched_routes = longest_match(routes)
-    for route in matched_routes:
-        print(json.dumps(route, default=json_serial_default))
